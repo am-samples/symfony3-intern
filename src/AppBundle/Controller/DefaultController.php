8fbb7474f96381b6b8c86a6f0c8942d0098bc1e2
Малоземов Александр <a.malozemov@artsofte.ru>
2017-05-16 15:36:43 +0500

    INTERN-29

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,17 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Task;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+
+
 
 class DefaultController extends Controller
 {
@@ -26,5 +34,96 @@ class DefaultController extends Controller
     {
         return $this->render('AppBundle:default:index.html.twig');
 
+    }
+
+    /**
+     * Matches /blog exactly
+     *
+     * @Route("/callback", name="callback_form")
+     */
+
+    public function callbackAction(Request $request)
+    {
+        $task = new Task();
+
+        $form = $this->createFormBuilder($task)
+            ->setAction($this->generateUrl('sendmail'))
+            ->add('name', TextType::class, array(
+                'label' => 'Имя: ',
+                'attr'  => array('class' => 'form-control', 'placeholder' => 'Введите ваше имя...')
+            ))
+
+            ->add('email', EmailType::class, array(
+                'label' => 'E-mail: ',
+                'attr'  => array('class' => 'form-control', 'placeholder' => 'Введите ваш e-mail...')
+            ))
+            ->add('comment', TextareaType::class, array(
+                'label' => 'Комментарий: ',
+                'attr'  => array('class' => 'form-control', 'placeholder' => 'Можете добавить комментарий...')
+            ))
+            ->add('send', SubmitType::class, array(
+                'label' => 'Отправить',
+                'attr'  => array('class' => 'btn btn-primary', 'style' => 'margin-top: 10px;')
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $task = $form->getData();
+
+            return $this->redirectToRoute('task_success');
+        }
+
+        return $this->render('AppBundle:callback:form.html.twig', array(
+            'form' => $form->createView(),
+        ));
+
+    }
+
+    /**
+     * Matches /blog exactly
+     *
+     * @Route("/sendmail", name="sendmail")
+     */
+
+    public function sendmailAction(Request $request)
+    {
+
+        $name = $request->get('name');
+        $email = $request->get('email');
+        $comment = $request->get('comment');
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Test e-mail')
+            ->setFrom('send@local.loc')
+            ->setTo('a.malozemov@tradedealer.ru')
+            ->setBody(
+                $this->renderView(
+                    'AppBundle:Emails:simple.html.twig',
+                    array(
+                        'name' => $name,
+                        'email' => $email,
+                        'comment' => $comment
+                    )
+                ),
+                'text/html'
+            );
+
+//        return $this->redirectToRoute('task_success');
+        return $this->render('AppBundle:callback:form_success.html.twig', array(
+            'name' => $name
+        ));
+    }
+
+    /**
+     * Matches /blog exactly
+     *
+     * @Route("/task_success", name="task_success")
+     */
+    public function task_successAction(Request $request)
+    {
+        return $this->render('AppBundle:callback:form_success.html.twig');
     }
 }
---
+++ src/AppBundle/Entity/Task.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Entity;
+
+class Task
+{
+    protected $task;
+
+    public function name()
+    {
+        return $this->task;
+    }
+
+    public function email()
+    {
+        return $this->task;
+    }
+
+    public function comment()
+    {
+        return $this->task;
+    }
+
+}
