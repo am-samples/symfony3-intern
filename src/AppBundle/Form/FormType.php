2dc26706f71ed09e4340e4a63f980f3d2da04464
Малоземов Александр <a.malozemov@artsofte.ru>
2017-05-18 10:50:43 +0500

    INTERN-29

--- src/AppBundle/Form/FormType.php
+++ src/AppBundle/Form/FormType.php
@@ -12,9 +12,11 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Validator\Constraints as Assert;
 
+/**
+ * Класс описывающий форму (поля)
+ */
 class FormType extends AbstractType
 {
-    // Класс описывающий форму (поля)
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
