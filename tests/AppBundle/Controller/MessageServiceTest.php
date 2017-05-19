d949524b8ac0857a272073c2ccd513c57bab7737
Малоземов Александр <a.malozemov@artsofte.ru>
2017-05-19 17:40:02 +0500

    INTERN-34

---
+++ tests/AppBundle/Controller/MessageServiceTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class MessageServiceTest extends WebTestCase
+{
+    public function testSend()
+    {
+
+        $client = self::createClient();
+
+        $message = $this->getMockBuilder('\Swift_Mailer', 'send')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $client->getContainer()->set('mailer', $message);
+
+        $srv = static::createClient();
+        $srv->getContainer()->get('app.message_service');
+//        $this->assertEquals($message, $message);
+//        $this->assertTrue($message->concreteMethod('send'));
+
+
+//        $me = $this->getMock('MessageService', array('send'));
+
+//        $message->expects($this->once())->method('send');
+//        $message->process();
+
+    }
+}
