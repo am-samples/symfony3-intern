e18f060f9c679b7d1fcbc80174e1cb3dd9e0fad1
Малоземов Александр <a.malozemov@artsofte.ru>
2017-06-09 16:53:39 +0500

    INTERN-50

--- app/scripts/up.sh
+++ app/scripts/up.sh
@@ -3,6 +3,7 @@
 composer install
 sudo chmod -R 777 var/cache
 php bin/console doctrine:migrations:migrate
+php bin/console fos:user:create adminuser --super-admin
 bower install
 php bin/console assets:install
 php bin/console assetic:dump --env=prod --no-debug
