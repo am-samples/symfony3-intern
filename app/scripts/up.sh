9873b67ca9136e760557ca2d6084277a26cb412c
Малоземов Александр <a.malozemov@artsofte.ru>
2017-06-09 16:56:28 +0500

    INTERN-50

--- app/scripts/up.sh
+++ app/scripts/up.sh
@@ -2,6 +2,7 @@
 
 composer install
 sudo chmod -R 777 var/cache
+sudo chmod -R 777 var/logs
 php bin/console doctrine:migrations:migrate
 php bin/console fos:user:create adminuser --super-admin
 bower install
