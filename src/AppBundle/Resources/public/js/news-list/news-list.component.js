1af9b254ef84db453021d431b3760ab0639a12b5
Малоземов Александр <a.malozemov@artsofte.ru>
2017-06-16 16:52:35 +0500

    INTERN-75-12

--- src/AppBundle/Resources/public/js/news-list/news-list.component.js
+++ src/AppBundle/Resources/public/js/news-list/news-list.component.js
@@ -7,12 +7,14 @@ component('newsList', {
     controller: function NewsListController($scope, $http) {
         var self = this;
 
+        var totalPages;
         $http.get('/app_dev.php/ru/getJsonNews').success(function(data) {
             $scope.news = data;
             $scope.totalItems = data.all;
             $scope.currentPage = 1;
             $scope.itemsPerPage = $scope.viewby;
             $scope.totalPages = Math.ceil($scope.totalItems / 10);
+            totalPages = $scope.totalPages;
             var it = $scope.locale;
             var item = $scope.currentPage;
         });
@@ -20,8 +22,14 @@ component('newsList', {
         $scope.setPage = function (pageNo) {
             $scope.currentPage = pageNo;
         };
-        $scope.disabled = false;
+
         $scope.viewby = function (countNews) {
+            if (countNews === 0 || countNews === totalPages) {
+                $scope.disabled = true;
+            }
+            else {
+                $scope.disabled = false;
+            }
             $http.get('/app_dev.php/ru/getJsonNews/'+countNews).success(function(data) {
                 $scope.news = data;
                 console.log('Вызвана функция, значение - ' + countNews);
